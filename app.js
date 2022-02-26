99c89b9899539654033f1bd7445d5704d0bd3a00
Harshit Paliwal <harshitpaliwal95@gmail.com>
2022-02-26 09:46:26 +0530

    Did: fp question

--- app.js
+++ app.js
@@ -16,13 +16,62 @@
 
 // console.log(newAge);
 
-const arr = [1, 5, 9, 11, 44, 66, 3, 7, 44, 6, 8];
+// const arr = [1, 5, 9, 11, 44, 66, 3, 7, 44, 6, 8];
 
-const moreThan10 = (num) => num > 10;
+// const moreThan10 = (num) => num > 10;
 
-const increaseByTwo = (num) => num + 2;
+// const increaseByTwo = (num) => num + 2;
 
-arr.map(increaseByTwo);
+// arr.map(increaseByTwo);
 
-arr.filter(lessThan10);
+// arr.filter(lessThan10);
 // console.log(arr);
+
+// const arr = [1, 5, 9, 11, 44, 66, 3, 7, 44, 6, 8];
+
+// const oddSum = (sum, num) => (sum % 2 === 0 ? sum : sum + num);
+// console.log(oddSum());
+
+// const arr = [1, 5, 9, 11, 44, 66, 3, 7, 44, 6, 8];
+
+// const oddEven = (odd, num) => {
+//   num % 2 === 0
+//     ? { ...obj, even: odd.even + num }
+//     : { ...obj, odd: odd.odd + num };
+// };
+
+// const obj = {
+//   odd: 0,
+//   even: 0,
+// };
+// console.log(arr.reduce(oddEven, obj));
+
+// const arr = [1, 5, 9, 11, 44, 66, 3, 7, 44, 6, 8];
+// // (previousvalue ,curruntValue)
+// const sumOdd = (num, sum) => {
+//   if (num % 2 == 0) {
+//     sum = sum + num;
+//   }
+// };
+// wrong wrong
+// console.log(arr.reduce(sumOdd));
+
+// const arr = [1, 2, 3, 4, 5, 6];
+// // const arr = [1, 1];
+
+// // const arr = [1, 5, 9, 11, 44, 66, 3, 7, 44, 6, 8];
+
+// const addOdd = (acc, cur) => (cur % 2 ? acc + cur : acc);
+// const addEven = (acc, curr) => (curr % 2 === 0 ? acc + curr : acc);
+
+// console.log(`sum of odd ${arr.reduce(addOdd, 0)}`);
+// console.log(`sum of even ${arr.reduce(addEven, 0)}`);
+
+// const arr = [9, 11, 44, 66, 7, 44, 6, 8, 100];
+
+// const MaxNum = (acc, cur) => (cur > acc ? (acc = cur) : acc);
+
+// console.log(arr.reduce(MaxNum));
+
+// const minNum = (acc, cur) => (cur < acc ? (acc = cur) : acc);
+// console.log(arr.reduce(minNum));
